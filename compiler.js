57f395d4a00466d855a5c94615d7dad514e6915b
CashMoney-Relentless <rydermathews07@gmail.com>
2025-12-01 13:27:47 -0500

    Add files via upload

---
+++ compiler.js
@@ -0,0 +1,9 @@
+const tokenizer = require('./tokenizer');
+const parser = require('./parser');
+const transformer = require('./transformer');
+module.exports = function compiler(input) {
+    const tokens = tokenizer(input);
+    const lispAST = parser(tokens);
+    const jsAST = transformer(lispAST);
+    return jsAST;
+}
